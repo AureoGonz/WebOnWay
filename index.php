e8ea4336dad5de43c8bc905d4ed16ab3d4a324d8
AureoGonz <aureogonz@hotmail.com>
2019-10-07 22:46:34 -0500

    Corrijo esqueleto para comportamiento del footer

--- index.php
+++ index.php
@@ -34,60 +34,31 @@
             </ul>
         </div>
     </nav>
-    <div class="container my-4 full-height">
+    <main class="container full-height my-4">
         <div class="row">
-            <div class="col-12 col-md-8">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
-                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
-                optio! Perferendis, consequuntur dolore?
-            </div>
+            <div class="col-12 col-md-8">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate illo aliquid optio ipsam magni porro qui dolorum aut corrupti incidunt unde impedit, commodi consequuntur voluptatum repudiandae accusantium repellat sequi neque!</div>
         </div>
         <div class="row">
-            <div class="col-12 col-md-8">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
-                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
-                optio! Perferendis, consequuntur dolore?
-            </div>
+            <div class="col-12 col-md-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia alias doloremque soluta? Eveniet, nobis assumenda? Distinctio, quos. Voluptatem debitis repellat commodi explicabo nemo, veniam minus provident ipsam odit ratione labore?</div>
         </div>
         <div class="row">
-            <div class="col-12 col-md-8">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
-                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
-                optio! Perferendis, consequuntur dolore?
-            </div>
+            <div class="col-12 col-md-8">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe, iure atque iusto totam aliquam soluta minima odio perspiciatis dolores molestiae praesentium quod debitis facere, corrupti in eaque eligendi ipsa natus!</div>
         </div>
         <div class="row">
-            <div class="col-12 col-md-8">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
-                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
-                optio! Perferendis, consequuntur dolore?
-            </div>
+            <div class="col-12 col-md-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa saepe suscipit, aliquam vero ipsum commodi nam facilis, assumenda quia consectetur eligendi? Aspernatur sapiente nulla officiis placeat maxime. Saepe, omnis expedita.</div>
         </div>
         <div class="row">
-            <div class="col-12 col-md-8">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
-                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
-                optio! Perferendis, consequuntur dolore?
+            <div class="col-12 col-md-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nulla enim incidunt nihil cum tenetur dolor ducimus ut itaque, est quasi expedita excepturi obcaecati ad quis aliquam iste, magnam atque?</div>
+        </div>
+    </main>
+    <footer class="footer mt-auto bg-dark pt-3 pb-2">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-12 col-md-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis quibusdam, vero quia incidunt doloremque exercitationem et. Est eos, minima sed accusantium sint officiis explicabo, totam laborum, hic ratione veniam eaque!</div>
+                <div class="col-12 col-md-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo illum est a placeat deserunt iste aspernatur rerum delectus. Esse suscipit veritatis beatae aliquam amet quod recusandae nulla saepe deleniti expedita?</div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12 col-md-8">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
-                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
-                optio! Perferendis, consequuntur dolore?
-            </div>
-        </div>
-    </div>
-    <div class="container-fluid mt-4 bg-dark">
-        <div class="row pt-2 justify-content-between">
-            <div class="col-12 col-md-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam repellat
-                aut accusamus? Minus dolore sit enim, omnis unde accusamus. Culpa eaque omnis quos exercitationem ullam
-                nihil, dolorum ipsum magni hic?</div>
-            <div class="col-12 col-md-5 pl-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo hic voluptas
-                dolor corrupti repellendus expedita consequuntur ab tempora laudantium? Magnam, pariatur? Earum placeat
-                aliquam sunt itaque, aperiam illo vero animi?</div>
-        </div>
-    </div>
+    </footer>
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/jquery.slimscroll.min.js"></script>
     <script type="text/javascript" src="js/popper.min.js"></script>
