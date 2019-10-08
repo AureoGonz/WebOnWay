aef5060910166d6f3ec12635abac1a24044b2670
AureoGonz <aureogonz@hotmail.com>
2019-10-07 22:18:21 -0500

    Integro esqueleto de la página

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <a class="navbar-brand" href="#">
+            <img src="img/wow.png" width="60" height="30" class="d-inline-block align-top" alt="">
+            WebOnWay
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Servicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container my-4 full-height">
+        <div class="row">
+            <div class="col-12 col-md-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
+                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
+                optio! Perferendis, consequuntur dolore?
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
+                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
+                optio! Perferendis, consequuntur dolore?
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
+                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
+                optio! Perferendis, consequuntur dolore?
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
+                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
+                optio! Perferendis, consequuntur dolore?
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
+                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
+                optio! Perferendis, consequuntur dolore?
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque expedita accusamus fugiat perferendis!
+                Explicabo est maiores temporibus ullam vitae, nemo minima molestias alias, facilis provident, minus
+                optio! Perferendis, consequuntur dolore?
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid mt-4 bg-dark">
+        <div class="row pt-2 justify-content-between">
+            <div class="col-12 col-md-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam repellat
+                aut accusamus? Minus dolore sit enim, omnis unde accusamus. Culpa eaque omnis quos exercitationem ullam
+                nihil, dolorum ipsum magni hic?</div>
+            <div class="col-12 col-md-5 pl-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo hic voluptas
+                dolor corrupti repellendus expedita consequuntur ab tempora laudantium? Magnam, pariatur? Earum placeat
+                aliquam sunt itaque, aperiam illo vero animi?</div>
+        </div>
+    </div>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.slimscroll.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
